ff1fdb96f05931a4b87592a5ae32b13ba5a45e73
John Snow <jpn1107@gmail.com>
2026-03-26 13:31:06 -0300

    Adição do toast

--- src/views/cliente/FormCliente.jsx
+++ src/views/cliente/FormCliente.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { Button, Container, Divider, Form, Icon } from 'semantic-ui-react';
 import MenuSistema from '../../MenuSistema';
+import { notifyError, notifySuccess } from '../../views/util/Util';
 
 export default function FormCliente () {
     const { state } = useLocation();
@@ -52,12 +53,26 @@ export default function FormCliente () {
 	
        if (idCliente != null) { //Alteração:
            axios.put("http://localhost:8080/api/cliente/" + idCliente, clienteRequest)
-           .then((response) => { console.log('Cliente alterado com sucesso.') })
-           .catch((error) => { console.log('Erro ao alter um cliente.') })
+           .then((response) => { notifySuccess('Cliente alterado com sucesso.') })
+           .catch((error) => { if (error.response.data.errors != undefined) {
+       		for (let i = 0; i < error.response.data.errors.length; i++) {
+	       		notifyError(error.response.data.errors[i].defaultMessage)
+	    	}
+	        } else {
+		            notifyError(error.response.data.message)
+	                }
+                })
        } else { //Cadastro:
            axios.post("http://localhost:8080/api/cliente", clienteRequest)
-           .then((response) => { console.log('Cliente cadastrado com sucesso.') })
-           .catch((error) => { console.log('Erro ao incluir o cliente.') })
+           .then((response) => { notifySuccess('Cliente cadastrado com sucesso.') })
+           .catch((error) => { if (error.response.data.errors != undefined) {
+       		for (let i = 0; i < error.response.data.errors.length; i++) {
+	       		notifyError(error.response.data.errors[i].defaultMessage)
+	    	}
+            } else {
+                notifyError(error.response.data.message)
+                    }
+             })
        }
     }
 
